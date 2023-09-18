04600467b32af0a00de738dde1368e48fdddd9e8
wikanadnyana <wikanadnyana44@gmail.com>
2023-09-18 19:56:54 +0800

    3

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const calculateButton = document.getElementById("calculate-button");
+  const calculateInput = document.getElementById("calculate-input");
+  const resultElement = document.querySelector(".container h1");
+
+  calculateButton.addEventListener("click", function () {
+    const inputValue = parseFloat(calculateInput.value);
+
+    if (isNaN(inputValue)) {
+      alert("Masukkan angka yang valid!");
+      return;
+    }
+
+    fetch("http://localhost:8080/api/calculate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ number: inputValue }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.calculation) {
+          resultElement.textContent = data.calculation;
+        } else {
+          alert("Tidak dapat menghitung akar kuadrat. Pastikan angka positif.");
+        }
+      })
+      .catch((error) => {
+        console.error("Terjadi kesalahan:", error);
+      });
+  });
+});
